19055f3d3617f67e27af9b4a50c400b72ea077f7
maxxcan <maxxcan@gmail.com>
2018-01-16 11:47:51 +0100

    Añadiendo las entradas ya realizadas

---
+++ introduccion-emacs-2/img/chuleta-emacs.mm
@@ -0,0 +1,202 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1250008638963" ID="Freemind_Link_1783418130" MODIFIED="1465761238642" TEXT="">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="freemind_butterfly"/>
+<node CREATED="1250008638978" HGAP="41" ID="Freemind_Link_1407362770" MODIFIED="1465761292902" POSITION="left" TEXT="Insertar comandos" VSHIFT="-146">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1250008638978" ID="Freemind_Link_619528792" MODIFIED="1465761238642" TEXT="Alt-x">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1250008681713" HGAP="67" ID="Freemind_Link_733778031" MODIFIED="1465761281358" POSITION="right" TEXT="Programa" VSHIFT="2227">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1465758966825" HGAP="74" ID="ID_1580263261" MODIFIED="1465761238641" TEXT="Ctrl-x + Ctrl-c" VSHIFT="-49">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465758994166" ID="ID_314568465" MODIFIED="1465761238641" TEXT="Salir del programa">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1465758946421" HGAP="41" ID="ID_1827454833" MODIFIED="1465761277962" TEXT="Ctrl-z" VSHIFT="-41">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250008689838" HGAP="30" ID="Freemind_Link_1268839072" MODIFIED="1465761238641" TEXT="En modo gr&#xe1;fico minimiza la ventana" VSHIFT="-21">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1465757816233" HGAP="39" ID="ID_232130124" MODIFIED="1465761238641" TEXT="En modo texto pone la aplicaci&#xf3;n en 2&#xba; plano" VSHIFT="29">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1465759272174" HGAP="129" ID="ID_1385590481" MODIFIED="1465761284390" POSITION="right" TEXT="Buffers" VSHIFT="-53">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1465759640022" ID="ID_1035551285" MODIFIED="1465761238640" TEXT="Ctrl+x">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465759659606" ID="ID_1876418836" MODIFIED="1465761238640" TEXT="Ctrl+f">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465759716230" ID="ID_573489501" MODIFIED="1465761238640" TEXT="Abrir o crear un fichero"/>
+</node>
+<node CREATED="1465759668854" ID="ID_243952464" MODIFIED="1465761238640" TEXT="Ctrl+s">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465759728502" ID="ID_561318546" MODIFIED="1465761238640" TEXT="Salvar el buffer"/>
+</node>
+<node CREATED="1465759682390" ID="ID_1375051617" MODIFIED="1465761238640" TEXT="Ctrl+w">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465759735110" ID="ID_1376029064" MODIFIED="1465761238640" TEXT="Salvar el buffer con otro nombre"/>
+</node>
+</node>
+</node>
+<node COLOR="#006699" CREATED="1250882962810" HGAP="88" ID="Freemind_Link_938941959" MODIFIED="1465761262502" POSITION="right" TEXT="Ventanas" VSHIFT="-74">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-6"/>
+<node CREATED="1250882988435" ID="Freemind_Link_1735498257" MODIFIED="1465761238638" TEXT="Ctrl-x">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250008765088" ID="Freemind_Link_239805374" MODIFIED="1465761238638" TEXT="0">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250009495103" ID="Freemind_Link_734365135" MODIFIED="1465761238638" TEXT="Cierra la ventana en las que est&#xe1;s"/>
+</node>
+<node CREATED="1250008768103" ID="Freemind_Link_1610960655" MODIFIED="1465761238638" TEXT="1">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250009471525" ID="Freemind_Link_365398498" MODIFIED="1465761238638" TEXT="Cierra las dem&#xe1;s menos en la que est&#xe1;s la cual maximiza"/>
+</node>
+<node CREATED="1250008715447" ID="Freemind_Link_1368556883" MODIFIED="1465761238638" TEXT="2">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250008799353" ID="Freemind_Link_818836062" MODIFIED="1465761238638" TEXT="Divide la ventana en 2 horizontales"/>
+</node>
+<node CREATED="1250008761338" ID="Freemind_Link_1016580363" MODIFIED="1465761238638" TEXT="3">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250008808603" ID="Freemind_Link_863322956" MODIFIED="1465761238638" TEXT="Divide la ventana en dos verticales"/>
+</node>
+<node CREATED="1250014200578" ID="Freemind_Link_175960433" MODIFIED="1465761238638" TEXT="o">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250014203953" ID="Freemind_Link_1555530666" MODIFIED="1465761238637" TEXT="Selecciona otra ventana"/>
+</node>
+</node>
+</node>
+<node COLOR="#006699" CREATED="1250026671531" HGAP="63" ID="Freemind_Link_841317777" MODIFIED="1465761238637" POSITION="right" TEXT="Buscar" VSHIFT="-2144">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-5"/>
+<node CREATED="1250009536150" ID="Freemind_Link_1625815071" MODIFIED="1465761238637" TEXT="Ctrl-r">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250009541103" ID="Freemind_Link_1906209121" MODIFIED="1465761238637" TEXT="Busqueda hacia arriba del documento">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1250009524041" ID="Freemind_Link_264283388" MODIFIED="1465761238637" TEXT="Ctrl-s">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250009529400" ID="Freemind_Link_1870499310" MODIFIED="1465761238637" TEXT="Busqueda hacia abajo del documento">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1250009878947" ID="Freemind_Link_734107338" MODIFIED="1465761238637" TEXT="Alt-%">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#006699" CREATED="1250009891010" ID="Freemind_Link_1586947009" MODIFIED="1465761238636" TEXT="Reemplazar">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250010721025" ID="Freemind_Link_1428667545" MODIFIED="1465761238636" TEXT="SPC/y">
+<node CREATED="1250025709937" ID="Freemind_Link_1791374198" MODIFIED="1465761238636" TEXT="Reemplaza este"/>
+</node>
+<node CREATED="1250011061760" ID="Freemind_Link_991369177" MODIFIED="1465761238636" TEXT="DEL/n">
+<node CREATED="1250025704703" ID="Freemind_Link_1983072312" MODIFIED="1465761238636" TEXT="Lo deja como est&#xe1;"/>
+</node>
+<node CREATED="1250025693828" ID="Freemind_Link_1368246659" MODIFIED="1465761238636" TEXT="!">
+<node CREATED="1250025701125" ID="Freemind_Link_1016605809" MODIFIED="1465761238636" TEXT="Reemplazar todo"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#006699" CREATED="1250026730953" ID="Freemind_Link_1316714317" MODIFIED="1465761238636" POSITION="left" TEXT="Cortar/Copiar/Pegar">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-4"/>
+<node CREATED="1250009703431" ID="Freemind_Link_550803874" MODIFIED="1465761238636" TEXT="Alt-w">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250009711853" ID="Freemind_Link_1868009979" MODIFIED="1465761238636" TEXT="Copiar">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1250009724635" ID="Freemind_Link_1611984736" MODIFIED="1465761238635" TEXT="Ctrl-w">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250009729572" ID="Freemind_Link_1655194732" MODIFIED="1465761238635" TEXT="Cortar">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1250009740181" ID="Freemind_Link_1837195260" MODIFIED="1465761238635" TEXT="Ctrl-y">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250009744744" ID="Freemind_Link_1246463824" MODIFIED="1465761238635" TEXT="Pelgar">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#006699" CREATED="1250026854703" HGAP="80" ID="Freemind_Link_1413238269" MODIFIED="1465761288574" POSITION="left" TEXT="Editando" VSHIFT="51">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1250026911812" ID="Freemind_Link_1785128537" MODIFIED="1465761238635" TEXT="Alt-g">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250027419500" ID="Freemind_Link_950504669" MODIFIED="1465761238634" TEXT="g">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250027421750" HGAP="49" ID="Freemind_Link_761889588" MODIFIED="1465761238634" TEXT="Ir a la l&#xed;nea"/>
+</node>
+</node>
+<node CREATED="1250629243281" ID="Freemind_Link_815876543" MODIFIED="1465761238634" TEXT="Ctrl-x">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250629247171" ID="Freemind_Link_1759238690" MODIFIED="1465761238634" TEXT="h">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1250629249781" HGAP="15" ID="Freemind_Link_194152244" MODIFIED="1465761238634" TEXT="Seleccionar todo"/>
+</node>
+</node>
+<node CREATED="1465760805382" ID="ID_987641483" MODIFIED="1465761238634" TEXT="Ctrl-a">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465760818070" ID="ID_1899621646" MODIFIED="1465761238634" TEXT="Ir al inicio de la l&#xed;nea">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1465760841158" ID="ID_1937388337" MODIFIED="1465761238633" TEXT="Ctrl-e">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465760866678" ID="ID_194808854" MODIFIED="1465761238633" TEXT="Ir al final de la l&#xed;nea">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1465760849078" ID="ID_1532866335" MODIFIED="1465761238633" TEXT="Alt-&lt;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465760881526" ID="ID_1474147134" MODIFIED="1465761238633" TEXT="Ir al inicio del buffer">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1465760858022" ID="ID_1544148290" MODIFIED="1465761238633" TEXT="Alt-&gt;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465760898902" ID="ID_656353840" MODIFIED="1465761238633" TEXT="ir al final del buffer">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1465760985031" ID="ID_1273110654" MODIFIED="1465761238633" TEXT="Alt-d">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465761004022" ID="ID_600879483" MODIFIED="1465761238632" TEXT="Borrar una palabra">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1465760997639" ID="ID_438383152" MODIFIED="1465761238632" TEXT="Ctrl-d">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465761010263" ID="ID_1531534432" MODIFIED="1465761238632" TEXT="Borrar una l&#xed;nea">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1465760745271" HGAP="93" ID="ID_195381430" MODIFIED="1465761238632" POSITION="left" TEXT="Deshacer/Rehacer" VSHIFT="62">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465761047671" ID="ID_100498637" MODIFIED="1465761238632" TEXT="Ctrl-/">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465761087367" ID="ID_688342536" MODIFIED="1465761238632" TEXT="Deshacer">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1465761077911" ID="ID_1634331282" MODIFIED="1465761238632" TEXT="Ctrl-?">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465761093671" ID="ID_1070946467" MODIFIED="1465761238631" TEXT="Rehacer">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</map>
